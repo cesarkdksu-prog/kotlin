9778890921b658ddb0988ca0d6999560d14fca5e
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-03-20 16:30:17 +0100

    [IR] Implement `IrSymbolFinderOverLinker`
    It will be used later on for `IrBuiltInsForLinker`.
    
    #KT-81659

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/SymbolFinderOverLinker.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/SymbolFinderOverLinker.kt
@@ -5,26 +5,35 @@
 
 package org.jetbrains.kotlin.backend.common
 
+import org.jetbrains.kotlin.backend.common.linkage.IrDeserializer
 import org.jetbrains.kotlin.backend.common.serialization.KotlinIrLinker
 import org.jetbrains.kotlin.ir.InternalSymbolFinderAPI
 import org.jetbrains.kotlin.ir.SymbolFinder
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
+import org.jetbrains.kotlin.ir.util.toIdSignature
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.ClassId
 
 @OptIn(InternalSymbolFinderAPI::class)
 class SymbolFinderOverLinker(private val linker: KotlinIrLinker) : SymbolFinder() {
     override fun findClass(classId: ClassId): IrClassSymbol? {
-        TODO("Not yet implemented")
+        val signature = classId.toIdSignature()
+        return linker.getSymbolAndPutIntoQueue(signature, kind = IrDeserializer.TopLevelSymbolKind.CLASS_SYMBOL) as? IrClassSymbol
     }
 
     override fun findFunctions(callableId: CallableId): Iterable<IrSimpleFunctionSymbol> {
-        TODO("Not yet implemented")
+        val signatures = linker.getAllMatchedSignatures(callableId, IrDeserializer.TopLevelSymbolKind.FUNCTION_SYMBOL)
+        return signatures.mapNotNull {
+            linker.getSymbolAndPutIntoQueue(it, kind = IrDeserializer.TopLevelSymbolKind.FUNCTION_SYMBOL) as? IrSimpleFunctionSymbol
+        }
     }
 
     override fun findProperties(callableId: CallableId): Iterable<IrPropertySymbol> {
-        TODO("Not yet implemented")
+        val signatures = linker.getAllMatchedSignatures(callableId, IrDeserializer.TopLevelSymbolKind.PROPERTY_SYMBOL)
+        return signatures.mapNotNull {
+            linker.getSymbolAndPutIntoQueue(it, kind = IrDeserializer.TopLevelSymbolKind.PROPERTY_SYMBOL) as? IrPropertySymbol
+        }
     }
 }
