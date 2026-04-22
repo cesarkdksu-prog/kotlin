a218748f0e81e6f57bb6b4414ec1bf7813e2fc39
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-04-22 13:21:38 +0200

    [K/N] Create IR for forward-declared classes w/o K1
    KonanForwardDeclarationModuleDeserializer would return IR for forward
    declared classes by first looking up their (synthetic) descriptor, then
    converting it into lazy IR via psi2ir. But because those classes are
    very simple, it is easier to just build the (non-lazy) IR for them
    directly.
    Which is what this commit does.
    
    It also removes the last place where lazy IR was being created in the
    K/N backend.
    
    #KT-86263 Fixed

--- compiler/ir/serialization.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrLinker.kt
+++ compiler/ir/serialization.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrLinker.kt
@@ -55,7 +55,7 @@ class KonanIrLinker(
     }
 
     private val forwardDeclarationDeserializer = forwardModuleDescriptor?.let {
-        KonanForwardDeclarationModuleDeserializer(it, this, stubGenerator)
+        KonanForwardDeclarationModuleDeserializer(it, this)
     }
 
     override val irMangler: KotlinMangler.IrMangler = KonanManglerIr
