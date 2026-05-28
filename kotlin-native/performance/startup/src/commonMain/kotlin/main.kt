633f8bbfc3e623e1d36bcd393fb316d354ff0320
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-05-28 11:32:16 +0200

    fixup! [K/N][performance] Migrate to kotlinx-benchmark in startup

--- kotlin-native/performance/startup/src/commonMain/kotlin/main.kt
+++ kotlin-native/performance/startup/src/commonMain/kotlin/main.kt
@@ -4,11 +4,15 @@
  */
 
 import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.BenchmarkTimeUnit
+import kotlinx.benchmark.Measurement
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.State
 import org.jetbrains.startup.*
 
 @State(Scope.Benchmark)
+// These benchmarks should not be repeated within a single process.
+@Measurement(time = 1, timeUnit = BenchmarkTimeUnit.NANOSECONDS)
 class Singleton {
     @Benchmark
     fun initialize() {
